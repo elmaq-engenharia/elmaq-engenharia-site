bd97fe70f1c6e1e11d7665838c62e5c2400df8f4
elmaq-engenharia <edsonengautomacao1@gmail.com>
2026-08-21 15:41:44 -0300

    Ativar seção de Rafael Augusto no menu e rolagem v22

--- script.js
+++ script.js
@@ -8,5 +8,7 @@
     s.onerror = () => done && done();
     document.head.appendChild(s);
   };
-  load('ceo-v21.js?v=21', () => load('script-core-v20.js?v=21'));
+  load('ceo-v21.js?v=22', () => {
+    load('rafael-v22.js?v=22', () => load('script-core-v20.js?v=22'));
+  });
 })();
